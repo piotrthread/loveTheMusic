9b65f05440fc2974d137a697951642a182e636e5
Piotr Niciejewski <piotrthread@gmail.com>
2018-10-10 20:33:47 +0200

    Initial Commit

---
+++ src/components/app/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import css from "./App.scss";
+
+class App extends React.Component{
+    render(){
+        return <React.Fragment>
+                
+        </React.Fragment>;
+    }
+}
+
+export default App;
