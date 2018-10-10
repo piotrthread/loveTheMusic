5d990b96a0fba642643e2483f5fb071e648783fe
Piotr Niciejewski <piotrthread@gmail.com>
2018-10-10 20:43:50 +0200

    updated Readme

---
+++ README.md
@@ -0,0 +1,1 @@
+https://piotrthread.github.io/loveTheMusic/
--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -4,7 +4,7 @@ import css from "./App.scss";
 class App extends React.Component{
     render(){
         return <React.Fragment>
-                
+                <h1>Hello World</h1>
         </React.Fragment>;
     }
 }
